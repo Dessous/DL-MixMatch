cffcb194bca315db017b282ee1e6916ea7f89577
juliatrofim <djuliya.trofimova@gmail.com>
2019-12-04 17:05:11 +0000

    Fix baseline config.

--- train_baseline.py
+++ train_baseline.py
@@ -5,6 +5,23 @@ from tensorboardX import SummaryWriter
 from wideresnet import WideResNet28
 from itertools import cycle
 
+
+def test(loader, model, criterion):
+    test_loss = 0.0
+    test_acc = 0.0
+    n = len(loader)
+
+    model.eval()
+    with torch.no_grad():
+        for batch in loader:
+            batch_size = batch[0].size(0)
+            pred = model(batch[0].cuda())
+            test_loss += criterion(pred, batch[1].cuda())
+            test_acc += (torch.argmax(pred, dim=1) == batch[1].cuda()).float().sum() / batch_size
+
+    return test_loss / n, test_acc / n
+
+
 def train_baseline(train_loader, val_loader, augmentor=None, lr=0.002, num_epoch=20, num_iter=1024):
     model = WideResNet28(10)
     model = model.cuda()
@@ -14,40 +31,29 @@ def train_baseline(train_loader, val_loader, augmentor=None, lr=0.002, num_epoch
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
     writer = SummaryWriter('logs-baseline')
-    loaders = {'train': cycle(train_loader), 'val': cycle(val_loader)}
+    train_loader_cycle = cycle(train_loader)
 
     for k in range(num_epoch):
         print("EPOCH ", k)
-        loss_val = 0.0
-        acc_val = 0.0
-        for stage, loader in loaders.items():
-            if stage == 'train':
-                model.train(True)
+        model.train(True)
+        for i in range(num_iter):
+            batch = next(train_loader_cycle)
+            optimizer.zero_grad()
+            pred = None
+            if augmentor is not None:
+                aug_batch = augmentor(batch[0])
+                pred = model(aug_batch.cuda())
             else:
-                model.eval()
-
-            for i in range(num_iter):
-                batch = next(loader)
-                optimizer.zero_grad()
-                pred = None
-                if augmentor is not None:
-                    aug_batch = augmentor(batch[0])
-                    pred = model(aug_batch.cuda())
-                else:
-                    pred = model(batch[0].cuda())
-                loss = criterion(pred, batch[1].cuda())
-                loss.backward()
-                if stage == 'train':
-                    optimizer.step()
-                    writer.add_scalar('Loss-' + stage, loss.item(), i + k * num_iter)
-                    writer.add_scalar('Accuracy-' + stage,
-                                      (torch.argmax(pred, dim=1) == batch[1].cuda()).float().sum() / 32,
-                                      i + k * num_iter)
-                else:
-                    loss_val += loss.item()
-                    acc_val += (torch.argmax(pred, dim=1) == batch[1].cuda()).float().sum() / 32
-            if stage == 'val':    
-                writer.add_scalar('Loss-' + stage, loss_val / len(val_loader), k)
-                writer.add_scalar('Accuracy-' + stage, acc_val / len(val_loader), k)
+                pred = model(batch[0].cuda())
+            loss = criterion(pred, batch[1].cuda())
+            loss.backward()
+            optimizer.step()
+            writer.add_scalar('loss-train', loss.item(), i + k * num_iter)
+            writer.add_scalar('Accuracy-train',
+                              (torch.argmax(pred, dim=1) == batch[1].cuda()).float().sum() / 32,
+                              i + k * num_iter)
+        loss_val, acc_val = test(val_loader, model, criterion)
+        writer.add_scalar('Loss-test', loss_val, k)
+        writer.add_scalar('Accuracy- test', acc_val, k)
 
 
